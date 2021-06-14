12c76a1c55276d9a7e7cb906ef202ca1d0ddf6c9
d-rov <74566223+d-rov@users.noreply.github.com>
2021-06-14 11:46:24 -0700

    Add files via upload

---
+++ DungeonTimer/Timer.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class Timer extends Component {
+    state = {
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 90
+    };
+
+    runTimer = () => {
+        this.setState({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            const newTime = this.state.timerTime - 0.01;
+            if (newTime >= 0) {
+                this.setState({
+                    timerTime: newTime
+                });
+            } else {
+                clearInterval(this.timer);
+                this.setState({ timerOn: false });
+                alert("Safe!");
+            }
+        }, 10);
+    };
+
+    render() {
+        const { timerTime } = this.state;
+        return (
+            <div>
+                <h1>{parseInt(timerTime,10)}</h1>
+                <button onClick={this.runTimer}>Start</button>
+            </div>
+        );
+    }
+}
+
+export default Timer;
